cc4ddf3ba2dfbcbf1faa5eaafc3a00b4a434342b
JustEnough2026 <kiss830226@gmail.com>
2026-07-17 12:21:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE="just-enough-v8";
+const CACHE="just-enough-v31";
 const FILES=[
 "./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg",
-"spicy_chicken.jpg","oil_quarter.jpg","oil_half.jpg",
-"noodles_small.jpg","noodles_large.jpg","oil_rice.jpg","hainan_rice.jpg"
+"spicy_chicken.jpg?v=31","oil_quarter.jpg?v=31","oil_half.jpg?v=31",
+"noodles_small.jpg?v=31","noodles_large.jpg?v=31","oil_rice.jpg?v=31","hainan_rice.jpg?v=31"
 ];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
